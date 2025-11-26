8079e01f6e21fe2d0b98bca97ff58f2ac1f54735
tolyashka <lushnikovt@gmail.com>
2025-11-26 22:07:09 +0300

    Initial Commit

---
+++ SubscriptionProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubscriptionProject
+//
+//  Created by Анатолий Лушников on 26.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
